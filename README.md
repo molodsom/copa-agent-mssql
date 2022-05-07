bb534e94a60ed6489e265016801af02ee97d69bf
victor <victor.ryzhikov@gmail.com>
2022-05-07 19:23:57 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# copa-agent-mssql
+
+## deployment
+1. Clone the repo
+2. Chdir into app directory: `cd copa-agent-mssql`
+3. Create a virtual environment: `virtualenv -p python3.10 venv`
+4. Activate virtual environment: `source venv/bin/activate`
+5. Install requirements: `pip install -r requirements.txt`
+6. Create env file: `cp .env.sample .env` and redefine vars
+7. Run `python3 main.py`
---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+import pyodbc
+from settings import config
+
+
+def get_cursor():
+    connect_params = {
+        "DRIVER": "{ODBC Driver 17 for SQL Server}",
+        "SERVER": config['MSSQL_HOST'],
+        "DATABASE": config['MSSQL_NAME'],
+        "UID": config['MSSQL_USER'],
+        "PWD": config['MSSQL_PASS'],
+    }
+    connection = pyodbc.connect(";".join([f"{k}={v}" for k, v in connect_params.items()]))
+    return connection.cursor()
+
+
+def request_data():
+    s = requests.Session()
+    s.headers = {"Authorization": config['COPA_API_TOKEN']}
+    return s.get(config['COPA_API_URL'] + "/globalspeed/users/").json()
+
+
+def main():
+    data = request_data()
+    inserts = [(4654, row['first_name'], row['last_name'], 50, 20, 0, 0, 0, 1, 3, 0) for row in data]
+    cursor = get_cursor()
+    cursor.executemany("""
+        INSERT INTO [dbo].[tblUser] (
+            [PIN], [Name], [Surname], [id_tblAdminSettings], [Role], [PasswordHashed],
+            [HasPicture], [HasProfilePicture], [IsActiv], [LanguageID], [IsTrainer]
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", inserts)
+    cursor.commit()
+
+
+if __name__ == '__main__':
+    main()
